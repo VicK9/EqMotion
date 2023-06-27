4d7e025031b3ed776abc7b5a24a0063f6bcdae5a
VicK9 <victor.kyriacou@student.uva.nl>
2023-06-27 19:43:03 +0200

    Formatted with black also not ignoring Hydrogens

--- md17/dataset_md17.py
+++ md17/dataset_md17.py
@@ -2,43 +2,53 @@ import numpy as np
 import torch
 import pickle as pkl
 import os
+
 # import networkx as nx
 # from networkx.algorithms import tree
 
 
-class MD17Dataset():
-    def __init__(self, partition, max_samples, data_dir, molecule_type,past_length=25,future_length=25):
-
-        full_dir = os.path.join(data_dir, molecule_type + '_' + partition + '.npy')
+class MD17Dataset:
+    def __init__(
+        self,
+        partition,
+        max_samples,
+        data_dir,
+        molecule_type,
+        past_length=25,
+        future_length=25,
+    ):
+        full_dir = os.path.join(data_dir, molecule_type + "_" + partition + ".npy")
 
         self.max_samples = int(max_samples)
-        self.data = self.load(data_dir,molecule_type,partition)
+        self.data = self.load(data_dir, molecule_type, partition)
+        self.num_nodes = self.data[0].shape[1]
         self.past_length = past_length
         self.future_length = future_length
         self.partition = partition
 
-    def load(self,data_dir,molecule_type,partition):
+    def load(self, data_dir, molecule_type, partition):
+        loc_full_dir = os.path.join(data_dir, molecule_type + "_" + partition + ".npy")
+        edge_full_dir = os.path.join(data_dir, molecule_type + "_" + "structure.npy")
+        z_full_dir = os.path.join(data_dir, molecule_type + "_" + "z.npy")
 
-        loc_full_dir = os.path.join(data_dir, molecule_type + '_' + partition + '.npy')
-        edge_full_dir = os.path.join(data_dir, molecule_type + '_' + 'structure.npy')
-
-        loc = np.load(loc_full_dir) # (B,T,N,3)
+        loc = np.load(loc_full_dir)  # (B,T,N,3)
         edge_attr = np.load(edge_full_dir)
-
-        loc = loc[:self.max_samples]
+        z = np.load(z_full_dir)  # (N)
+        loc = loc[: self.max_samples]
 
         loc = torch.Tensor(loc)
         edge_attr = torch.Tensor(edge_attr)
 
-        loc = loc.transpose(1,2)
+        loc = loc.transpose(1, 2)
         vel = torch.zeros_like(loc)
-        vel[:,:,1:] = loc[:,:,1:] - loc[:,:,:-1]
-        vel[:,:,0] = vel[:,:,1]
+        vel[:, :, 1:] = loc[:, :, 1:] - loc[:, :, :-1]
+        vel[:, :, 0] = vel[:, :, 1]
 
-        edge_attr = edge_attr[None,:,:].repeat(loc.shape[0],1,1)
+        edge_attr = edge_attr[None, :, :].repeat(loc.shape[0], 1, 1)
+        z = z[None, :].repeat(loc.shape[0], 1)
         # edge_attr = self.get_molecule_structure(loc[0])
 
-        return (loc, vel, edge_attr)
+        return (loc, vel, edge_attr, z)
 
     def set_max_samples(self, max_samples):
         self.max_samples = int(max_samples)
@@ -48,13 +58,21 @@ class MD17Dataset():
         return self.data[0].size(1)
 
     def __getitem__(self, i):
-        loc, vel, edge_attr = self.data
+        loc, vel, edge_attr, z = self.data
         loc, vel, edge_attr = loc[i], vel[i], edge_attr[i]
+        # loc shape: (N,T,3)
         frame_0 = self.past_length
-        frame_T = self.past_length + self.future_length
-
+        frame_T = self.past_length + 1
+        # feature = torch.cat([loc, vel], dim=-1)
+        # # Use torch.unfold to split the trajectory into overlapping windows
+        # feature = (
+        #     feature.unfold(1, frame_T, 1)  # (N, T-W+1, 6, W)
+        #     .permute(1, 0, 3, 2)  # (T-W+1, N, W, 6)
+        #     .reshape(-1, frame_T, feature.shape[-1])  # (T-W+1 * N, seq_len, 6)
+        #     .contiguous()
+        # )
         # if self.training:
-        return loc[:,0:frame_0], vel[:,0:frame_0], edge_attr, loc[:,frame_0:frame_T]
+        return loc[:, 0:frame_0], vel[:, 0:frame_0], edge_attr, loc[:, frame_0:frame_T]
         # else:
         #     return loc[:,0:frame_0], vel[:,0:frame_0], edge_attr, loc[:,frame_0:frame_T]
         # if self.training:
